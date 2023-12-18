7b3214db9a0fb6385311bfbf093c98775eb91d91
Nathan <nathan.arriesgado0g@gmail.com>
2023-12-18 18:10:30 +0800

    [Misc][Nathan] Cleaned up look of DB setup file

--- server/database/mongo.js
+++ server/database/mongo.js
@@ -1,12 +1,8 @@
 import { MongoClient, ServerApiVersion } from 'mongodb'
 import 'dotenv/config'
 
-const uri = 
-"mongodb+srv://" + 
-process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD + 
-"@timetopia.t10971p.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv:// ${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@timetopia.t10971p.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -17,9 +13,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } catch (error) {
