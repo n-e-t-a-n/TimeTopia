0ffd8e8efd6cdd8ed81d937383e845f72cf7c151
Nathan <nathan.arriesgado0g@gmail.com>
2023-12-19 17:48:24 +0800

    [Feat][Nathan] Added basic login functionality endpoint

--- server/express-server.js
+++ server/express-server.js
@@ -1,51 +1,54 @@
 import express from 'express';
 import client from './database/mongo.js'
+import cors from 'cors'
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
+app.use(cors())
 
-app.get('/', async (_, res) => {
-    try {
-      const collection = await client.db('sample_analytics').collection('customers');
-      const docs = await collection.find({  });
-  
-      if (docs.length === 0) {
-        res.send({ message: 'No customer found with that username' });
-      } else {
-        res.send({ docs });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ message: 'Internal server error' });
-    }
-  });
+app.post('/login', async(req, res) => {
+  try {
+    const collection = await client.db('users').collection('clients');
+    const docs = await collection.find({ email: req.body.email }).toArray();
 
-  app.post('/clients', async (req, res) => {
-    try {
-      const { username, name, email, ...additionalData } = req.body;
-      const collection = await client.db('users').collection('clients');
-  
-      const newClient = {
-        username,
-        name,
-        email,
-        ...additionalData
-      };
-  
-      const result = await collection.insertOne(newClient);
-  
-      if (result.acknowledged) {
-        res.status(201).send({ message: 'Client created successfully!' });
-      } else {
-        throw new Error('Failed to create client.'); // Handle the error appropriately
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ message: 'Internal server error.' });
+    if (docs.length === 0) return res.send({ status: 404, message: 'No user found with that email' });
+    
+    if (docs[0]["password"] != req.body.password) return res.send({ status: 401, message: 'Incorrect password. '})
+
+    return res.send({ status: 200, message: docs[0] })
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Internal server error' });
+  }
+})
+
+app.post('/clients', async (req, res) => {
+  try {
+    const { username, name, email, ...additionalData } = req.body;
+    const collection = await client.db('users').collection('clients');
+
+    const newClient = {
+      username,
+      name,
+      email,
+      ...additionalData
+    };
+
+    const result = await collection.insertOne(newClient);
+
+    if (result.acknowledged) {
+      res.status(201).send({ message: 'Client created successfully!' });
+    } else {
+      throw new Error('Failed to create client.'); // Handle the error appropriately
     }
-  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Internal server error.' });
+  }
+});
 
 app.listen( port ,()=>{
     console.log('server is running at port number 3000')
