d703cfc7420e92521e2152b0e7f8a4e19e07b189
Nathan <nathan.arriesgado0g@gmail.com>
2023-12-20 00:37:53 +0800

    [Misc][Nathan] Modified formatting for passing of parameters to login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,28 +3,25 @@ import { useAuth } from '../providers/AuthContext';
 
 const Login = () => {
   const { login } = useAuth();
-  const [formData, setFormData] = useState({ username: '', password: '' });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
-  };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    login(formData);
+    login({ email, password });
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label>
-        Username:
-        <input type="text" name="username" value={formData.username} onChange={handleChange} />
+        Email:
+        <input type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
       </label>
       <br />
       <label>
         Password:
-        <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
       </label>
       <br />
       <button type="submit">Login</button>
