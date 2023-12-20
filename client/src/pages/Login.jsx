a3afe0a898fdb98b382d78c986ca29de51a15ce8
Nathan <nathan.arriesgado0g@gmail.com>
2023-12-20 14:28:32 +0800

    [Feat][Nathan] Added formik forms and yup validation

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,31 +1,52 @@
-import React, { useState } from 'react';
-import { useAuth } from '../providers/AuthContext';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+import { useAuth } from '../providers/AuthContext.jsx'
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email address').required('Email is required'),
+  password: Yup.string().required('Password is required'),
+});
+
+const initialValues = {
+  email: '',
+  password: '',
+};
 
 const Login = () => {
   const { login } = useAuth();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    login({ email, password });
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      <br />
-      <button type="submit">Login</button>
-    </form>
+    <div>
+      <h1>Login</h1>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          login(values);
+          setSubmitting(false);
+        }}
+      >
+        <Form>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <Field type="email" id="email" name="email" />
+            <ErrorMessage name="email" component="div" className="error" />
+          </div>
+
+          <div>
+            <label htmlFor="password">Password:</label>
+            <Field type="password" id="password" name="password" />
+            <ErrorMessage name="password" component="div" className="error" />
+          </div>
+
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </Form>
+      </Formik>
+    </div>
   );
 };
 
