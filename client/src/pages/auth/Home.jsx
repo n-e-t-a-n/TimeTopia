a6faf0e6aef2f8aef7759095e603f5584e4e948e
Nathan <nathan.arriesgado0g@gmail.com>
2023-12-22 01:15:15 +0800

    [Feat][Nathan] Integrated graphql read event query to home scheduler

--- client/src/pages/auth/Home.jsx
+++ client/src/pages/auth/Home.jsx
@@ -1,18 +1,67 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useAuth } from '../../providers/AuthContext';
-import { BryntumCalendar } from '@bryntum/calendar-react';
+import { BryntumCalendar, BryntumCalendarProjectModel } from '@bryntum/calendar-react';
 import '@bryntum/calendar/calendar.stockholm.css';
 
+const query = `
+  query events($email: String!) {
+    events(email: $email) {
+      _id
+      name
+      startDate
+      endDate
+    }
+  }
+`;
 
+const variables = {
+    email: 'test@test.com',
+};
 
-export default () => {
+const Home = () => {
     const { logout } = useAuth();
 
+    const calendar = useRef();
+    const project = useRef();
+
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        const fetchEvents = async () => {
+            try {
+                const response = await fetch('http://localhost:4000/graphql', {
+                    method: 'POST',
+                    headers: {
+                      'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        query,
+                        variables
+                    }),
+                });
+
+                const data = await response.json();
+                setEvents(data["data"]["events"]);
+            } catch (error) {
+                console.error('Error fetching events:', error);
+            }
+        };
+
+        fetchEvents();
+    }, []);
+    
     return (
         <>
-          <BryntumCalendar />
+            <BryntumCalendarProjectModel
+                ref={project}
+                events={events}
+            />
 
-          <button onClick={logout}>Logout</button>
+            <BryntumCalendar ref={calendar} project={project} />
+
+            <button onClick={logout}>Logout</button>
         </>
     );
-}
+};
+
+export default Home;
