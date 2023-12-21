5c96be5edba34a4ba131ccbec3a92d6718a845e0
Zixonia <21102386@usc.edu.ph>
2023-12-22 00:49:26 +0800

    Login Front End Design 1.0

--- client/src/pages/pre-auth/test.jsx
+++ client/src/pages/pre-auth/test.jsx
@@ -3,7 +3,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css"
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import {MDBContainer, MDBCol, MDBRow, MDBBtn, MDBInput} from 'mdb-react-ui-kit';
+import {MDBContainer, MDBCol, MDBRow, MDBBtn, MDBIcon, MDBInput} from 'mdb-react-ui-kit';
 
 import { useAuth } from '../../providers/AuthContext.jsx';
 
@@ -32,29 +32,26 @@ const Login = ({ setIsLogin }) => {
   const { login } = useAuth();
 
   return (
-    <MDBContainer fluid className="p-3 my-5 h-100">
+    <MDBContainer fluid className="p-3 my-5 h-custom">
 
-      <MDBRow className="h-100 align-items-center">
+      <MDBRow>
 
         {/* Left column container with background */}
-        <MDBCol md='6' className="order-1 order-md-1">
+        <MDBCol md='6' className="order-2 order-md-1">
           <img
-            src="../../img/Time_topia_smol.png"
+            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
             className="img-fluid"
             alt="Sample image"
           />
         </MDBCol>
 
         {/* Right column container */}
-        <MDBCol md='6' className="order-2 order-md-2">
-        <div className="relative mb-6" data-te-input-wrapper-init>
+        <MDBCol md='6' className="order-1 order-md-2">
+
+          <div className="relative mb-6" data-te-input-wrapper-init>
             <div className="text-center">
-              <h1 className="mt-1 ">Sign In Here</h1>
-            {/* Separator between social media sign in and email/password sign in */}
-            <div className="divider d-flex align-items-center my-3">
-                <hr className="w-100" />
-              </div>
-        </div>
+              <h1 className="mt-1 mb-5 pb-1">Sign In Here</h1>
+            </div>
             <Formik
               initialValues={initialValues}
               validationSchema={validationSchema}
@@ -70,6 +67,10 @@ const Login = ({ setIsLogin }) => {
               }}
             >
               <Form>
+                {/* Separator between social media sign in and email/password sign in */}
+                <div className="divider d-flex align-items-center my-4">
+                  <p className="text-center fw-bold mx-3 mb-0"></p>
+                </div>
                 {/* Email input */}
                 <div className="relative mb-6" data-te-input-wrapper-init>
                   <Field component={CustomInput} label='Email address' type="email" id="email" name="email" />
