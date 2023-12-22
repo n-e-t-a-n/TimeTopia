253642d19115b3a115cbaf665a651aadca6ac211
Nathan <nathan.arriesgado0g@gmail.com>
2023-12-22 22:39:03 +0800

    [Feat][Nathan] Added resolver for updateEvent mutation

--- server/database/gql/resolvers.js
+++ server/database/gql/resolvers.js
@@ -45,7 +45,46 @@ const resolvers = {
         throw new Error('Failed to create event');
       }
     },
+    updateEvent: async (_, args) => {
+      try {
+        const { email, id, ...updatedEventData } = args;
+        const updatedEvent = { id, ...updatedEventData };
+    
+        const currentUser = await client.db('timetopia').collection('users').findOne({ email });
+    
+        if (!currentUser) {
+          console.log("User not found with the provided email");
+          return false;
+        }
+    
+        // Find the index of the event to update in the schedules array
+        const eventIndex = currentUser.schedules.findIndex(event => event.id === id);
 
+        if (eventIndex === -1) {
+          console.log("Event not found with the provided ID");
+          return false;
+        }
+    
+        // Update the event at the found index
+        currentUser.schedules[eventIndex] = updatedEvent;
+
+    
+        const result = await client.db('timetopia').collection('users').updateOne(
+          { email: email },
+          { $set: { schedules: currentUser.schedules } }
+        );
+    
+        if (result.modifiedCount === 1) {
+          return true;
+        }
+    
+        console.log('User not found or event not updated');
+        return false;
+      } catch (error) {
+        console.error('Error updating event:', error);
+        throw new Error('Failed to update event');
+      }
+    },
   },
 };
 
